c92c24cc5d75051cc099e31483e1aabd9b3c0cb4
user <mr.sergei82@mail.ru>
2025-08-31 14:03:41 +0300

    UTF_8

---
+++ user/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,37 @@
+package service;
+
+import com.example.user.model.User;
+import com.example.user.repository.UserRepository;
+import com.example.user.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void getAllUsers_shouldReturnListOfUsers() {
+
+    }
+
+    //простой тест для проверки
+    @Test
+    void simpleTest() {
+        assertTrue(true, "Простой тест должен проходить");
+    }
+}
